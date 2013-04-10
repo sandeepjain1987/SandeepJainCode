a5f99c6c3aefd7809dddb11bb69e4e36b6be6953
Sandeep Jain <sandeepjainusa@gmail.com>
2013-04-10 03:31:07 -0400

    GroupMessenger

--- SimpleMessenger/gen/edu/buffalo/cse/cse486586/simplemessenger/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.buffalo.cse.cse486586.simplemessenger;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
